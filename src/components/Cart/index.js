4389f1fe1ed2f3b0480941c31f570e2691fcfa99
kiran kumar <kirankumar76k>
2022-04-14 12:36:15 +0000

    commit changes

---
+++ src/components/Cart/index.js
@@ -0,0 +1,18 @@
+// Write your JS code here
+// Write your JS code here
+import Header from '../Header'
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="product-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+        alt="cart"
+        className="prod-img"
+      />
+    </div>
+  </>
+)
+export default Products
